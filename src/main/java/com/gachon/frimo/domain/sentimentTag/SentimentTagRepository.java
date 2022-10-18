acf384f057a23e45e0123b790fb8257d29ab0809
vivi108 <118ssun@naver.com>
2022-10-18 22:18:41 +0900

    add : UserRepository

--- src/main/java/com/gachon/frimo/domain/sentimentTag/SentimentTagRepository.java
+++ src/main/java/com/gachon/frimo/domain/sentimentTag/SentimentTagRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SentimentTagRepository extends JpaRepository<SentimentTag, Long>{
-    public Optional<SentimentTag> findBySentPk (Long sentPk);
+public interface SentimentTagRepository extends JpaRepository<SentimentTag, Long> {
+    public SentimentTag findBySentPk(Long sentPk);
 }
--- src/main/java/com/gachon/frimo/domain/user/UserRepository.java
+++ src/main/java/com/gachon/frimo/domain/user/UserRepository.java
@@ -1,5 +1,15 @@
 package com.gachon.frimo.domain.user;
 
-public class UserRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    public User findByUserPk(Long userPk);
+
+    public Optional<User> findByUserId(String userid);
+
+
 }
