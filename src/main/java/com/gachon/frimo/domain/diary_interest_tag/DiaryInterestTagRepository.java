02a3a846a1365f616eddd60db0a4e6927e5b1a47
vivi108 <118ssun@naver.com>
2022-10-18 22:12:58 +0900

    add : DiaryInterestTagRepository

---
+++ src/main/java/com/gachon/frimo/domain/diary_interest_tag/DiaryInterestTagRepository.java
@@ -0,0 +1,17 @@
+package com.gachon.frimo.domain.diary_interest_tag;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gachon.frimo.domain.sentimentTag.SentimentTag;
+
+
+@Repository
+public interface DiaryInterestTagRepository extends JpaRepository<DiaryInterestTag, Long>{
+    public Optional<DiaryInterestTag> findByCategory(String category);
+    public Optional<DiaryInterestTag> findByTagContent(String tagContent);
+    public Optional<DiaryInterestTag> findBySentimentTag(SentimentTag sentimentTag);
+    
+}
