23d9b006d1b726784fff5fccb2108e4c5eb65f9e
vivi108 <118ssun@naver.com>
2022-12-07 12:52:04 +0900

    fix : cascade

--- src/main/java/com/gachon/frimo/domain/diary/Diary.java
+++ src/main/java/com/gachon/frimo/domain/diary/Diary.java
@@ -22,8 +22,6 @@ import java.time.LocalDateTime;
 import javax.persistence.*;
 import java.util.List;
 
-
-
 @Getter
 @Entity
 @Component
@@ -46,36 +44,38 @@ public class Diary extends BaseTimeEntity implements Serializable {
     @Column(name = "image_path", length = 100)
     private String imagePath;
 
-    @Column(name = "author")
-    private User author; // userPk
-
     @Column(name = "date_created")
     private LocalDateTime dateCreated;
 
     @Column(name = "date_created_year")
     private int dateCreatedYear;
 
-    @Column(name= "date_created_month")
+    @Column(name = "date_created_month")
     private int dateCreatedMonth;
 
     @Setter
     @Column(name = "main_sent") // 제일 빈도 수 많은 감정을 뽑기 -> 별도의 함수 만들기
     private int mainSent;
 
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "author")
+    private User author; // userPk
+
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "diary")
     private List<DiaryInterestTag> tags;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy ="diary")
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "diary")
     private List<CommentAI> comments;
 
     @Builder
-    public Diary(String diaryTitle, String diaryContent, String imagePath, User author,LocalDateTime dateCreated, int mainSent, int dateCreatedYear, int dateCreatedMonth) {
+    public Diary(String diaryTitle, String diaryContent, String imagePath, User author, LocalDateTime dateCreated,
+            int mainSent, int dateCreatedYear, int dateCreatedMonth) {
         this.diaryContent = diaryContent;
         this.diaryTitle = diaryTitle;
         this.imagePath = imagePath;
         this.author = author;
         this.dateCreated = dateCreated;
-        this.mainSent= mainSent;
+        this.mainSent = mainSent;
         this.dateCreatedYear = dateCreatedYear;
         this.dateCreatedMonth = dateCreatedMonth;
     }
